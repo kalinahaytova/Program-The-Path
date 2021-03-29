09362fe101f8085608ffe7eef75b1f1dbd4684bd
Kalina <salvatorekalina@gmail.com>
2021-03-29 22:12:09 +0300

    Added code

--- game.js
+++ game.js
@@ -2,6 +2,8 @@
 //global variable, that stores the program instructions
 var instructions = [];
 
+var currentInstruction = 0;
+
 var map = [
     [2, 0, 0],
     [1, 1, 0],
@@ -16,6 +18,13 @@ var map2 = [
     [0, 1, 1, 1, 0, 1],
     [0, 0, 3, 0, 0, 1]
 ];
+
+var levelArray = [map, map2];
+var player;
+
+//this will be our displayed map 
+var currentLevelIndex = 0;
+var currentLevel = initLevel(currentLevelIndex);
 
 //get all the buttons instances and attach click event
 var allButtons = document.querySelectorAll("button");
@@ -33,6 +42,10 @@ function onButtonClick(event) {
     var span = document.createElement("SPAN");
     span.innerHTML = buttonText;
     instructionSection.appendChild(span);
+
+    if (buttonText === "Start") {
+        playMove();
+    }
 }
 
 function renderMap(map) {
@@ -91,4 +104,84 @@ function hideMessage() {
     h1.style.display = "none";
 }
 
-renderMap(map);
+function findPlayerPosition(map) {
+    for (var i = 0; i < map.length; i++) {
+        for (var j = 0; j < map[i].length; j++) {
+            if (map[i][j] === 2) {
+                return { x: j, y: i };
+            }
+        }
+    }
+}
+
+function playMove() {
+    var oldPosition = { x: player.x, y: player.y }
+    //reading the next instruction from the instruction array
+    var instruction = instructions[currentInstruction];
+    currentInstruction++;
+    console.log(instruction);
+
+    if (instruction === undefined) {
+        displayMessage("Game Over!");
+    }
+
+    //check if the move is valid
+    switch (instruction) {
+        case "Left":
+            player.x -= 1;
+            break;
+        case "Right":
+            player.x += 1;
+            break;
+        case "Down":
+            player.y += 1;
+            break;
+        case "Up":
+            player.y -= 1;
+            break;
+        case "Start":
+            displayMessage("Game over!");
+            break;
+    }
+
+    //update the map array
+    try {
+        if (currentLevel[player.y][player.x] === 0) {
+            currentLevel[oldPosition.y][oldPosition.x] = 0;
+            currentLevel[player.y][player.x] = 2;
+            setTimeout(playMove, 1000);
+        } else if (currentLevel[player.y][player.x] === 3) {
+            displayMessage("You Win!");
+            currentLevel[oldPosition.y][oldPosition.x] = 0;
+            currentLevel[player.y][player.x] = 2;
+            if (currentLevelIndex === levelArray.length - 1) {
+                displayMessage("You finished the game!");
+            } else {
+                displayMessage("You win the level! Loading next one...");
+                currentLevelIndex++;
+                setTimeout(function () {
+                    currentLevel = initLevel(currentLevelIndex);
+                }, 2000);
+            }
+        } else {
+            displayMessage("Game Over!");
+        }
+    } catch {
+        displayMessage("Game Over!");
+    }
+
+    //render the map
+    renderMap(currentLevel);
+}
+
+function initLevel(levelIndex) {
+    var level = levelArray[levelIndex];
+    renderMap(level);
+    player = findPlayerPosition(level);
+    instructions = [];
+    currentInstruction = 0;
+    var instructionSection = document.querySelector("section");
+    instructionSection.innerHTML = "";
+    hideMessage();
+    return level;
+} 
